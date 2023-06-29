34a05d7e3ce99f3da6fa1a0aa435e4c85b644645
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-29 04:30:21 +0100

    added seeding data for flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,18 @@ airports.each do |airport|
         code: airport['code']
     )
 end
+    
+airport_ids = Airport.pluck(:id)
+
+75.times do
+    departure_airport_id = airport_ids.sample
+    arrival_airport_id = (airport_ids - [departure_airport_id]).sample
+    start = Time.now + rand(1..30).days
+    duration = rand(60..240)
+    Flight.create!(
+        departure_airport_id: departure_airport_id,
+        arrival_airport_id: arrival_airport_id,
+        start: start,
+        duration: duration
+    )
+end
