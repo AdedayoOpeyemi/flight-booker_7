9ab2e71dbdb68cf554e7ee16cdc307f0f5c5be61
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-06-29 01:58:49 +0100

    seed data for airport added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+airports_csv = File.read('db/data/airport_list.csv')
+airports = CSV.parse(airports_csv, headers: true)
+
+airports.each do |airport|
+    Airport.create!(
+        name: airport['name'],
+        city: airport['city'],
+        code: airport['code']
+    )
+end
