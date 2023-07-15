f12506b4798026bbbf8ddbe7d67bf00b49ff2937
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-07-16 00:24:54 +0100

    added available dates to flight form dte dropdown

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,10 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all.order(:name)
+    # @days = Flight.all.pluck(:start).strftime("%B, %d, %Y").uniq
+    @days = Flight.pluck(:start).map { |date| date.strftime('%d/%m/%Y') }.uniq
   end
 
   def search
-    # @flights = Flights.find.where()
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,20 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+  def self.distinct_dates
+    distinct_dates = Flight.select("DATE(datetime) as date").distinct.pluck(:start).strftime("%m/%d/%Y")
+  end
+
+  def departure_date_formatted
+    departure_date.strftime("%B, %d, %Y")
+  end
+
+  def departure_time_formatted
+    departure_time.strftime("%l:%M %P")
+  end
+
+  # def event_date_formatted
+  #   event_date.strftime("%m/%d/%Y")
+  # end
 end
