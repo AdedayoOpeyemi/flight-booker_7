9fdd44294a737a11defd99c65577cbdd4dbc1b99
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-07-18 21:52:37 +0100

    search and select flight

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
   # Defines the root path route ("/")
   root to: "flights#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'flights', to: 'flights#index'
   get 'flights/search', to: 'flights#search'
+  # get 'bookings/new', to: 'bookings#new'
 end
